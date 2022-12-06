f837ebda7ed012ee1476e9707860000dd5aa846c
0709nvanh <dungtmph12934@fpt.edu.vn>
2022-12-06 23:16:04 +0700

    DTN-284-be-api-message-group

--- controllers/message.js
+++ controllers/message.js
@@ -13,3 +13,38 @@ export const getMessageByGroup = async (req, res) => {
     res.json(error);
   }
 };
+
+export const createMessage = async (req, res) => {
+  try {
+    const message = await Message(req.body).save();
+    res.json(message);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+export const updateMessage = async (req, res) => {
+  try {
+    const message = await Message.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    ).populate("userId")
+    .populate("classId").exec();
+    res.json(message);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+
+export const removeMessage = async (req, res) => {
+  try {
+    const message = await Message.findOneAndDelete(
+      { _id: req.params.id }
+    ).exec();
+    res.json(message);
+  } catch (error) {
+    res.json(error);
+  }
+};
